d7afba67fb19354aa5a9eb6154c84b944c9f29ce
Gabriel Silva Magalhães <gabiles278@gmail.com>
2026-09-13 19:26:28 -0300

    feat(bullmq-test): implement worker processor and complete job lifecycle scenarios

--- examples/bullmq-test/index.ts
+++ examples/bullmq-test/index.ts
@@ -1,1 +1,3 @@
-console.log("Hello via Bun!");
+import { worker } from "./src/workers/test-worker";
+
+await worker.run();
--- examples/bullmq-test/src/init-job.ts
+++ examples/bullmq-test/src/init-job.ts
@@ -1,22 +1,55 @@
 import { Queue } from "bullmq";
 import { redis } from "./server/config/redis";
+import { cleanAllJobsFromQueue } from "./service/clean-jobs";
 import { JobTypes } from "./shared/enums/jobs-types";
+import { QueueNames } from "./shared/enums/queue-names";
 
 // const connection = await getRedisConnection();
 
-const testQueue = new Queue("test-queue", {
+const testQueue = new Queue(QueueNames.TestQueue, {
   connection: redis,
 });
 
 const initJobs = async () => {
+  await cleanAllJobsFromQueue(testQueue);
+
   const quicklyJob = await testQueue.add(JobTypes.QuicklyJob, {
-    mesage: "Job 1 rápido iniciado",
+    message: "Job rápido iniciado",
+    apiToken: "TEST-API-TOKEN",
+    password: "TEST-DB-PASSWORD",
   });
 
-  console.log(quicklyJob.data);
-  // await testQueue.add(JobTypes.QuicklyJob, { mesage: "Job 2 rápido iniciado" });
+  const slowlyJob = await testQueue.add(JobTypes.SlowlyJob, {
+    message: "Job lento iniciado",
+  });
+
+  const errorJob = await testQueue.add(
+    JobTypes.ErrorJob,
+    {
+      message: "Job de erro iniciado",
+    },
+    {
+      attempts: 3,
+      backoff: {
+        type: "exponential",
+        delay: 5000,
+      },
+    },
+  );
+
+  const delayJob = await testQueue.add(
+    JobTypes.DelayJob,
+    {
+      message: "Job com delay iniciado",
+    },
+    { delay: 10000 },
+  );
 
   console.info("Jobs iniciados com sucesso!");
+
+  // Close connections
+  await testQueue.close();
+  await redis.quit();
 };
 
 await initJobs();
